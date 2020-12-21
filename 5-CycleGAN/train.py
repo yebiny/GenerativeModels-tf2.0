2701d873d72a8b49044d2fe64322b43be6cfea33
yebiny <yebinyoun@gmail.com>
2020-12-21 16:49:32 +0900

    add some options

--- 5-CycleGAN/train.py
+++ 5-CycleGAN/train.py
@@ -7,16 +7,19 @@ from tensorflow.keras.utils import plot_model
 class TrainCycleGAN():
     def __init__(self, dataset, save_path, img_shape=(128,128,3), batch_size=64):
         
+        # set init vars
         self.dataset = dataset
         self.img_shape = img_shape
         self.batch_size = batch_size
         self.disc_patch = (8, 8, 1)
         self.save_path = save_path 
-        
+       
+        # set cyclegan
         CycleGAN = BuildCycleGAN(img_shape)
         self.gene_ab, self.gene_ba, self.disc_a, self.disc_b, self.cyclegan = CycleGAN.build_cyclegan()
         self.cyclegan.summary()
         
+        # draw model images
         plot_model(self.gene_ab, to_file=save_path+'/gene.png', show_shapes=True)        
         plot_model(self.disc_a, to_file=save_path+'/disc.png', show_shapes=True)        
         plot_model(self.cyclegan, to_file=save_path+'/cyclegan.png', show_shapes=True)        
@@ -43,13 +46,21 @@ class TrainCycleGAN():
                               optimizer=optimizer)        
         
     def train(self, epochs):
-        y0, y1 = self.make_constant()
+        
+        # set data
         train_a, train_b, test_a, test_b = self.make_datasets()
         sample_a, sample_b = next(iter(train_a)), next(iter(train_b))
-
-        for epoch in range(epochs):
-            for batch_idx, (imgs_a, imgs_b) in enumerate(tf.data.Dataset.zip(
-                                                  (train_a, train_b))):
+        y0, y1 = self.make_constant()
+        
+        # train
+        history = {'epoch':[], 'disc_loss':[], 'gene_loss':[]}
+        generate_img(self.gene_ab, self.gene_ba, sample_a, sample_b, self.save_path+'/geneImg_0' )        
+        for epoch in range(1, epochs+1):
+            
+            gene_loss=disc_loss=0
+            print("* epoch {}/{}".format(epoch, epochs)))
+            for batch_idx, (imgs_a, imgs_b) in enumerate(tf.data.Dataset.zip((train_a, train_b))):
+            
                 fake_b = self.gene_ab.predict(imgs_a)
                 fake_a = self.gene_ba.predict(imgs_b)
                 
@@ -63,19 +74,26 @@ class TrainCycleGAN():
                 db_loss_fake = self.disc_b.train_on_batch(fake_b, y0)
                 db_loss = 0.5*np.add(db_loss_real, db_loss_fake)
                 
-                d_loss = 0.5 *np.add(da_loss, db_loss)
+                d_loss = 0.5*np.add(da_loss, db_loss)
                 
-                self.disc_a.trainable = True
-                self.disc_b.trainable = True
+                self.disc_a.trainable = False
+                self.disc_b.trainable = False
                 g_loss = self.cyclegan.train_on_batch([imgs_a, imgs_b],
                                                      [y1, y1, imgs_a, imgs_b, imgs_a, imgs_b]) 
-                            
-                #if batch_idx%10==0: 
-                generate_img(self.gene_ab, self.gene_ba, sample_a, sample_b, 
-                             self.save_path+'/geneImg_%i_%i'%(epoch, batch_idx) )        
-                print('* %i - %i  :'%(epoch, batch_idx), da_loss, db_loss, g_loss)
+                
+                disc_loss = disc_loss + d_loss
+                gene_loss = gene_loss + g_loss
+
+            print('* d_loss: ', disc_loss, 'g_loss: ' , g_loss)
+            history['epoch'].append(epoch)
+            history['d_loss'].append(disc_loss)
+            history['g_loss'].append(gene_loss)
+            generate_img(self.gene_ab, self.gene_ba, sample_a, sample_b, 
+                         self.save_path+'/geneImg_%i_%i'%(epoch, batch_idx) )        
 
             self.disc_a.save(self.save_path+'/disc_a_%i.h5'%epoch)  
             self.disc_b.save(self.save_path+'/disc_b_%i.h5'%epoch)  
             self.gene_ab.save(self.save_path+'/gene_ab_%i.h5'%epoch)  
-            self.gene_ba.save(self.save_path+'/gene_ba_%i.h5'%epoch)  
+            self.gene_ba.save(self.save_path+'/gene_ba_%i.h5'%epoch) 
+
+        return history
